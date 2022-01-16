c1d06bd080533c38fa7182e57dae9c69862a1cdd
Murilo Sopi <dev.murilosopi@gmail.com>
2022-01-16 18:15:24 -0300

    Create the timer pause and resume functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,34 +5,26 @@ const clock = document.querySelector('.time-remaining');
 const desiredTime = document.querySelector('input.timers-duration');
 let secondsRemaining;
 let timer;
+const btnResume = document.createElement('button');
 
 document.addEventListener('click', (e) => {
     if (e.target == btnReset) {
-        clearTimer();
+        resetTimer();
+        setDefaultState();
     };
     if (e.target == btnPause) {
-        console.log('It Works');
+        pauseTimer()
     };
     if (e.target == btnStart) {
-        startTimer();
+        resetTimer();
+        setDefaultState();
+        startTimer(desiredTime.value);
     };
+
+    if (e.target == btnResume) {
+        resumeTimer();
+    }
 });
-
-function clearTimer() {
-    clearInterval(timer);
-    return clock.textContent = '00:00:00';
-}
-
-function startTimer() {
-    secondsRemaining = desiredTime.value;
-    timer = setInterval(() => {
-        if (secondsRemaining == 0) {
-            clearTimer();
-        }
-        clock.textContent = getTimeFromSeconds(secondsRemaining);
-        secondsRemaining--;
-    }, 1000);
-}
 
 function getTimeFromSeconds(seconds) {
     const msRemaining = seconds * 1000;
@@ -44,4 +36,49 @@ function getTimeFromSeconds(seconds) {
         minute: '2-digit',
         second: '2-digit',
     });
+};
+
+function startTimer(time) {
+    secondsRemaining = time;
+    timer = setInterval(() => {
+        if (secondsRemaining == 0) {
+            resetTimer();
+        }
+        updateClock(getTimeFromSeconds(secondsRemaining));
+        secondsRemaining--;
+    }, 1000);
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    secondsRemaining = 0;
+    updateClock(getTimeFromSeconds(0));
+}
+
+function updateClock(info) {
+    clock.textContent = info;
+};
+
+function pauseTimer() {
+    if (secondsRemaining > 0) {
+        clearInterval(timer);
+        btnPause.style.display = 'none';
+        createResumeButton();
+    };
+};
+
+function resumeTimer() {
+    startTimer(secondsRemaining);
+    setDefaultState();
+};
+
+function createResumeButton() {
+    btnResume.classList.add('resume');
+    btnResume.textContent = 'Resume';
+    btnPause.insertAdjacentElement('afterend', btnResume);
+};
+
+function setDefaultState() {
+    btnResume.remove();
+    btnPause.style.display = 'inline-flex';
 };
